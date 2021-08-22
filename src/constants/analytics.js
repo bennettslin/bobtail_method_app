778d6a69c6afcb3d056a9ffed07a2047352b74c6
Bennett Lin <bennettslin@gmail.com>
2021-08-21 19:10:31 -0700

    Update GA account.

--- src/constants/analytics.js
+++ src/constants/analytics.js
@@ -1,4 +1,4 @@
-export const GA_ACCOUNT = 'G-Z28Y4T51Z8'
+export const GA_ACCOUNT = 'G-5FZ1HNQ4NB'
 
 export const ANALYTICS__FAILURE = 'analyticsFailure'
 export const ANALYTICS__SUCCESS = 'analyticsSuccess'
