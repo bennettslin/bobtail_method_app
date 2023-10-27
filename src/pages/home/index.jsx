1afa7bea96e8de03e4f9ca3268bcb0d604442e80
Bennett Lin <bennettslin@gmail.com>
2023-10-27 15:14:26 -0700

    More changes.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -16,13 +16,11 @@ const Home = () => (
             >
                 <Markdown>
                     {`
-*22 July 2022*
-
 ### Who we're looking for
 
 Hi, I'm Bennett from the [Bobtail Yearlings](https://www.bobtailyearlings.com)! We're looking for a business partner who recognizes the opportunity presented by *Bobtail Method*, our songwriting book. I wrote the manuscript that you see here as a proof of concept to win over the indie labels. I never published it on my own— not just because a songwriting book from an unknown band has little value— but also because it probably works best as an interactive tutorial and compositional tool.
 
-If you're an entrepreneur with the vision to understand what *Bobtail Method* can be worth with your investment and support, and you're interested in partnering with us to turn *Bobtail Method* into an online app for web and native platforms, please get in touch!
+If you're an entrepreneur with the vision to understand what *Bobtail Method* can be worth with your investment and support, and you're interested in partnering with us to turn it into an online app for web and mobile platforms, please get in touch!
                     `}
                 </Markdown>
                 <ContactEmail />
@@ -34,9 +32,9 @@ Back when [*Yearling's Bobtail*](https://www.yearlingsbobtail.com) was a standal
 
 But okay, I tried to do things their way for a while. I tried the hustle. And… it didn't feel good at all. I wasn't improving creatively; I wasn't advancing the art form. In fields where genuine innovation happens, fake work that wastes your time is known as "bullshit"— I don't like to curse, but that's the technical term. And my bullshit detector kept going off. It kept telling me that this was the wrong way to go. So I decided not to do the hustle. And anyone who cares about the art form wouldn't want me to, either.
 
-Instead of building a fanbase, then, I wrote *Bobtail Method* and sent out copies of the manuscript to all the indie labels. The point was to show them that: it takes effort to compose strong melodies and chord progressions, and this is where much of *my* time and energy goes; I care about helping other artists improve, and I can help the ones on *their* rosters; and the book can promote the album, and vice versa, because I use my own songs as musical examples.
+Instead of building a fanbase, then, I wrote *Bobtail Method* and sent out copies of the manuscript to all the indie labels. The point was to show them that: it takes effort to compose strong melodies and chord progressions, and this is where much of *my* time and energy goes; I care about helping other artists improve, and I can help the ones on *their* rosters; and the book can promote the album, because it uses my own songs as musical examples.
 
-I just figured that, if you're a label, then surely this is what you'd rather see a band do. Obviously, bands should innovate; building a fanbase is just an option you offer to those who can't. Right? Well… no. The indie labels still insisted that I build a fanbase on my own. But since I didn't see anything wrong with my reasoning, I just assumed I hadn't met the threshold. If I stuck with my strategy of always trying to advance the art form, then eventually I'd meet it, however many years it took.
+I just figured that, if you're a label, then surely this is what you'd rather see a band do. Obviously, bands should innovate; building a fanbase is just an option you offer to those who can't. Right? Well… no. The indie labels still insisted that I build a fanbase on my own. But I didn't see anything wrong with my reasoning, so I just assumed I hadn't met the threshold. If I stuck with my strategy of trying to advance the art form, then eventually I'd meet it, however many years it took.
 
 So I invented [amnesticism](https://digital.lib.washington.edu/researchworks/handle/1773/23566), a method for tonal composition in multidimensional spaces; I patented [Bobtail Dominoes](https://bobtaildominoes.com), a musical tile game; and I coded and illustrated the audio webcomic to go along with *Yearling's Bobtail*. But sadly, the indie labels dismissed every one of these projects as well. Finally, it hit me: I'll never meet the threshold, because there isn't one. It's not possible to avoid the hustle by advancing the art form instead, because the indie labels are loyal to the *hustle*. And because they're seen as the "good guys," they'll never be called out for this.
 
